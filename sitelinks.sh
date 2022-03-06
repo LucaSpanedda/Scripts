18cb26ea4e9ffdac4fe84a6b71ce15c8aa3b7ab2
Luca_Spanedda <stigmadubstep@gmail.com>
2022-03-06 15:27:43 +0100

    Delete sitelinks.sh

--- sitelinks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script for download all the link from a website page
-echo insert the website link:
-read link
-lynx -dump -listonly $link >> sitelinks.txt
-echo all the links have been downloaded into a file
-
