a9b9188d73df30966bdb95dd84ef8b6760c7ca00
Luca_Spanedda <stigmadubstep@gmail.com>
2022-03-22 10:48:00 +0100

    Update mygitdownload.sh

--- mygitdownload.sh
+++ mygitdownload.sh
@@ -1,22 +1,44 @@
 #!/bin/sh
-
 #My Repositories : scripts for download:
 
-git clone https://github.com/LucaSpanedda/faustlibraries.git ;
-git clone https://github.com/LucaSpanedda/Guida_Primi_Codici_in_FAUST.git ;
-git clone https://github.com/LucaSpanedda/Scripts.git ;
-git clone https://github.com/LucaSpanedda/slll.pd.git ;
-git clone https://github.com/LucaSpanedda/LucaSpanedda.github.io.git ;
-git clone https://github.com/LucaSpanedda/Musical_studies_of_Dynamical_and_Complex_Systems.git;
-git clone https://github.com/LucaSpanedda/Graphviz_DOT.git ;
-git clone https://github.com/LucaSpanedda/modernCV-LaTex.git ;
-git clone https://github.com/LucaSpanedda/Terminal_Tedium_Patches.git ;
-git clone https://github.com/LucaSpanedda/LucaSpanedda.git ;
-git clone https://github.com/LucaSpanedda/Guida_alla_Scrittura_in_Latex.git ;
-git clone https://github.com/LucaSpanedda/Guida_alla_Notazione_in_LilyPond.git ;
-git clone https://github.com/LucaSpanedda/Tecniche_di_Registrazione_e_Riproduzione_Audio_in_FAUST.git ;
-git clone https://github.com/LucaSpanedda/pdmexpy.git;
-git clone https://github.com/LucaSpanedda/Teensy-Euroshield_Patches.git ;
-git clone https://github.com/LucaSpanedda/Sintesi_per_modelli_fisici_in_FAUST.git ;
-git clone https://github.com/LucaSpanedda/Riverberazione_Digitale_in_FAUST.git ;
-git clone https://github.com/LucaSpanedda/Digital_Oscillators_in_Pure_Data.git ;
+
+sudo rm -r  Guida_alla_Scrittura_in_Latex;
+sudo rm -r  Musical_studies_of_Dynamical_and_Complex_Systems;
+sudo rm -r  faust;
+sudo rm -r  Riverberazione_Digitale_in_FAUST;
+sudo rm -r  Scripts;
+sudo rm -r  LucaSpanedda.github.io;
+sudo rm -r  Tecniche_di_Registrazione_e_Riproduzione_Audio_in_FAUST;
+sudo rm -r  faustlibraries;
+sudo rm -r  slll.pd;
+sudo rm -r  Guida_Primi_Codici_in_FAUST;
+sudo rm -r  Graphviz_DOT;
+sudo rm -r  Terminal_Tedium_Patches;
+sudo rm -r  LucaSpanedda;
+sudo rm -r  Guida_alla_Notazione_in_LilyPond;
+sudo rm -r  pdmexpy;
+sudo rm -r  Teensy-Euroshield_Patches;
+sudo rm -r  Sintesi_per_modelli_fisici_in_FAUST;
+sudo rm -r  fc2003dsaae2;
+sudo rm -r  Digital_Oscillators_in_Pure_Data;
+
+
+sudo git clone  https://github.com/LucaSpanedda/Guida_alla_Scrittura_in_Latex.git;
+sudo git clone  https://github.com/LucaSpanedda/Musical_studies_of_Dynamical_and_Complex_Systems.git;
+sudo git clone  https://github.com/LucaSpanedda/faust.git;
+sudo git clone  https://github.com/LucaSpanedda/Riverberazione_Digitale_in_FAUST.git;
+sudo git clone  https://github.com/LucaSpanedda/Scripts.git;
+sudo git clone  https://github.com/LucaSpanedda/LucaSpanedda.github.io.git;
+sudo git clone  https://github.com/LucaSpanedda/Tecniche_di_Registrazione_e_Riproduzione_Audio_in_FAUST.git;
+sudo git clone  https://github.com/LucaSpanedda/faustlibraries.git;
+sudo git clone  https://github.com/LucaSpanedda/slll.pd.git;
+sudo git clone  https://github.com/LucaSpanedda/Guida_Primi_Codici_in_FAUST.git;
+sudo git clone  https://github.com/LucaSpanedda/Graphviz_DOT.git;
+sudo git clone  https://github.com/LucaSpanedda/Terminal_Tedium_Patches.git;
+sudo git clone  https://github.com/LucaSpanedda/LucaSpanedda.git;
+sudo git clone  https://github.com/LucaSpanedda/Guida_alla_Notazione_in_LilyPond.git;
+sudo git clone  https://github.com/LucaSpanedda/pdmexpy.git;
+sudo git clone  https://github.com/LucaSpanedda/Teensy-Euroshield_Patches.git;
+sudo git clone  https://github.com/LucaSpanedda/Sintesi_per_modelli_fisici_in_FAUST.git;
+sudo git clone  https://github.com/LucaSpanedda/fc2003dsaae2.git;
+sudo git clone  https://github.com/LucaSpanedda/Digital_Oscillators_in_Pure_Data.git;
