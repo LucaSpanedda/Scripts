6baa25799db9188cea178ca57128cf552c9f2d8f
Luca_Spanedda <stigmadubstep@gmail.com>
2022-04-17 21:04:52 +0200

    Update mygitdownload.sh

--- mygitdownload.sh
+++ mygitdownload.sh
@@ -1,27 +1,9 @@
 #!/bin/sh
 #My Repositories : scripts for download:
 
-
-sudo rm -r  Guida_alla_Scrittura_in_Latex;
-sudo rm -r  Musical_studies_of_Dynamical_and_Complex_Systems;
-sudo rm -r  faust;
-sudo rm -r  Riverberazione_Digitale_in_FAUST;
-sudo rm -r  Scripts;
-sudo rm -r  LucaSpanedda.github.io;
-sudo rm -r  Tecniche_di_Registrazione_e_Riproduzione_Audio_in_FAUST;
-sudo rm -r  faustlibraries;
-sudo rm -r  slll.pd;
-sudo rm -r  Guida_Primi_Codici_in_FAUST;
-sudo rm -r  Graphviz_DOT;
-sudo rm -r  Terminal_Tedium_Patches;
-sudo rm -r  LucaSpanedda;
-sudo rm -r  Guida_alla_Notazione_in_LilyPond;
-sudo rm -r  pdmexpy;
-sudo rm -r  Teensy-Euroshield_Patches;
-sudo rm -r  Sintesi_per_modelli_fisici_in_FAUST;
-sudo rm -r  fc2003dsaae2;
-sudo rm -r  Digital_Oscillators_in_Pure_Data;
-
+sudo rm -r  my-git;
+sudo mkdir  my-git;
+cd my-git;
 
 sudo git clone  https://github.com/LucaSpanedda/Guida_alla_Scrittura_in_Latex.git;
 sudo git clone  https://github.com/LucaSpanedda/Musical_studies_of_Dynamical_and_Complex_Systems.git;
@@ -42,3 +24,5 @@ sudo git clone  https://github.com/LucaSpanedda/Teensy-Euroshield_Patches.git;
 sudo git clone  https://github.com/LucaSpanedda/Sintesi_per_modelli_fisici_in_FAUST.git;
 sudo git clone  https://github.com/LucaSpanedda/fc2003dsaae2.git;
 sudo git clone  https://github.com/LucaSpanedda/Digital_Oscillators_in_Pure_Data.git;
+
+cd ..;
