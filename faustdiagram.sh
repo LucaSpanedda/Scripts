7b3f9fd29c87d3facbb8f927fa1c4f7073f0481f
Luca_Spanedda <stigmadubstep@gmail.com>
2022-06-11 19:56:01 +0200

    Update faustdiagram.sh

--- faustdiagram.sh
+++ faustdiagram.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Script for plot a Faust Block Diagram in a single page 
-
-echo insert the file.dsp for plot the full Block Diagram in a single page -svg
+# Then convert as pdf with rsvg
+# Install: sudo apt-get install librsvg2-bin
+ 
+echo insert the file name without .dsp extension and plot the full Block Diagram
 read faustfile
-faust -sd -sn -fc 1000 -svg $faustfile 
+faust -sd -sn -fc 1000 -svg $faustfile.dsp
+
+cd $faustfile-svg
+rsvg-convert -f pdf -o process.pdf process.svg
+cd .. 
