aaae960a696cbda70a9f7a00a9a6ef17b9c1be1f
Luca_Spanedda <stigmadubstep@gmail.com>
2022-07-17 21:36:17 +0200

    Update mygitdownload.sh

--- mygitdownload.sh
+++ mygitdownload.sh
@@ -1,28 +1,4 @@
-#!/bin/sh
-#My Repositories : scripts for download:
+#!/bin/bash
+#cloning all the repository of an user
 
-sudo rm -r  my-git;
-sudo mkdir  my-git;
-cd my-git;
-
-sudo git clone  https://github.com/LucaSpanedda/Guida_alla_Scrittura_in_Latex.git;
-sudo git clone  https://github.com/LucaSpanedda/Musical_studies_of_Dynamical_and_Complex_Systems.git;
-sudo git clone  https://github.com/LucaSpanedda/faust.git;
-sudo git clone  https://github.com/LucaSpanedda/Riverberazione_Digitale_in_FAUST.git;
-sudo git clone  https://github.com/LucaSpanedda/Scripts.git;
-sudo git clone  https://github.com/LucaSpanedda/LucaSpanedda.github.io.git;
-sudo git clone  https://github.com/LucaSpanedda/Tecniche_di_Registrazione_e_Riproduzione_Audio_in_FAUST.git;
-sudo git clone  https://github.com/LucaSpanedda/faustlibraries.git;
-sudo git clone  https://github.com/LucaSpanedda/slll.pd.git;
-sudo git clone  https://github.com/LucaSpanedda/Guida_Primi_Codici_in_FAUST.git;
-sudo git clone  https://github.com/LucaSpanedda/Graphviz_DOT.git;
-sudo git clone  https://github.com/LucaSpanedda/Terminal_Tedium_Patches.git;
-sudo git clone  https://github.com/LucaSpanedda/LucaSpanedda.git;
-sudo git clone  https://github.com/LucaSpanedda/Guida_alla_Notazione_in_LilyPond.git;
-sudo git clone  https://github.com/LucaSpanedda/pdmexpy.git;
-sudo git clone  https://github.com/LucaSpanedda/Teensy-Euroshield_Patches.git;
-sudo git clone  https://github.com/LucaSpanedda/Sintesi_per_modelli_fisici_in_FAUST.git;
-sudo git clone  https://github.com/LucaSpanedda/fc2003dsaae2.git;
-sudo git clone  https://github.com/LucaSpanedda/Digital_Oscillators_in_Pure_Data.git;
-
-cd ..;
+sudo curl -s https://api.github.com/users/LucaSpanedda/repos | grep \"clone_url\" | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
